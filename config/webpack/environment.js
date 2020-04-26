970fd90892079bba1bcb70ea3510c23de2014d9b
dova3187 <dovanny3187@gmail.com>
2020-04-25 19:01:31 -0500

    add jQuery, jquery-ui, jquery-ujs to unobtrusive scripting

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,22 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack');
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+        $: 'jquery',
+        JQuery: 'jquery',
+        jquery: 'jquery',
+        'window.Tether': "tether",
+        Popper: ['popper.js', 'default'], // for Bootstrap 4
+    })
+)
+
+const aliasConfig = {
+    'jquery': 'jquery/src/jquery',
+    'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+
+};
+
+environment.config.set('resolve.alias', aliasConfig);
+
 module.exports = environment
