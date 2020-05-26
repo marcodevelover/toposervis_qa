52d26384d26c913e1bb18fd72241fb0ec03cd80d
dova3187 <dovanny3187@gmail.com>
2020-05-26 01:21:00 -0500

    dashboard querys

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,11 @@
+class Admin::DashboardController < ApplicationController
+  def page_name
+     @page_name = "Dashboard"
+  end	
+  def index
+  	@item_sum = Item.sum(:total)
+  	@quotation_count = Quotation.count
+  	@order_service_count = ServiceOrder.count
+  	@order_count = Order.count
+  end
+end
