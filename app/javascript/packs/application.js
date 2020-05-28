154a6aafbe0dcd1e3b2b4b7588632dfb1472986f
dova3187 <dovanny3187@gmail.com>
2020-05-28 17:02:09 -0500

    test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,9 +29,6 @@ global.$ = $
 global.jQuery = $
 
 require('jquery-ui');
-// jquery-ui theme
-require.context('file-loader?name=[path][name].[ext]&context=node_modules/jquery-ui-dist!jquery-ui-dist', true,    /jquery-ui\.css/ );
-require.context('file-loader?name=[path][name].[ext]&context=node_modules/jquery-ui-dist!jquery-ui-dist', true,    /jquery-ui\.theme\.css/ );
 
 
 
