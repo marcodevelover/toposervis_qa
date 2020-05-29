f5054173ab4d2f995dd17ffd89ab71b66287b7fa
dova3187 <dovanny3187@gmail.com>
2020-05-28 23:53:07 -0500

    improve pending "app/models/application_record.rb"

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -5,7 +5,7 @@ class ApplicationRecord < ActiveRecord::Base
 
   before_validation :erase , on: :delete 
   #before_validation :erase_sale, on: :delete, if: Proc.new { self.sale.present? || self.diagnosis.sale.present? }
-  before_validation :delivered , on: :output, if: Proc.new { self.diagnosis.present? && self.diagnosis.sale.present? && state == "sold" } 
+  before_validation :delivered , on: :output, if: Proc.new { self.diagnosis.present? && self.diagnosis.sale.present? && self.state == "sold" } 
   def erase	
   	self.deleted_at=Time.now
   end  
@@ -16,6 +16,7 @@ class ApplicationRecord < ActiveRecord::Base
   #end  
 
   def delivered	
-  	self.state="delivered"
+    @service_order = ServiceOrder.find_by(id: self.id)
+    @service_order.update(state: "delivered")    
   end  
 end
