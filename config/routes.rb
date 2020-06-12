e6704607e248c8d0a8225d12e53a8af85f387598
dova3187 <dovanny3187@gmail.com>
2020-06-11 21:40:43 -0500

    custom template errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  get '/404', to: "errors#not_found"
+  get '/422', to: "errors#unacceptable"
+  get '/500', to: "errors#internal_error"  
   resources :use_of_cfdis
   namespace :admin do
     root to: "dashboard#index"
