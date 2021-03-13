eec9e86d00881c5476f6263473da16afec05c7c5
dova3187 <dovanny3187@gmail.com>
2021-03-13 13:04:32 -0600

    fix

--- app/controllers/admin/purchases_controller.rb
+++ app/controllers/admin/purchases_controller.rb
@@ -106,7 +106,7 @@ class Admin::PurchasesController < ApplicationController
   end
 
   def product_variants
-    @q = ProductVariant.active.ransack(params[:q])
+    @q = ProductVariant.ransack(params[:q])
     @product_variants = @q.result(distinct: true)
     total_count = @product_variants.count
     respond_to do |format|
