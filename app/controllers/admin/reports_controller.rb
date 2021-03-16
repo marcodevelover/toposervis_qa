ac98c72ee1182d1c9f1a98970282d02c85aef513
dova3187 <dovanny3187@gmail.com>
2021-03-16 13:56:28 -0600

    report in and out

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -178,4 +178,60 @@ class Admin::ReportsController < ApplicationController
             format.xlsx {render xlsx: "reports", template: "admin/products/kardex.xlsx.axlsx"}
     end 
   end
+
+  def in_total
+#@q = Product.joins(product_variants: :stock_item).where('stock_items.stock != ?', 0).search(params[:q])
+
+    @page_description = 'Entradas'
+    @q = Product.ransack(params[:q])
+    unless params[:q].nil?
+      created_at_gt = params[:q][:created_at_gteq]
+      created_at_lt = params[:q][:created_at_lteq]
+
+      if params[:q][:created_at_gteq].present?
+        params[:q][:created_at_gteq] = Time.zone.parse(params[:q][:created_at_gteq]) rescue ""
+      end
+
+      if params[:q][:created_at_lteq].present?
+        params[:q][:created_at_lteq] = Time.zone.parse(params[:q][:created_at_lteq]) rescue ""
+      end
+      
+      @collection = @q.result(distinct: true).
+      select('products.*, stock_movements.folio, stock_movements.description, stock_movements.stock, stock_movements.quantity, stock_movements.cost_price, stock_movements.deleted_at, stock_movements.created_at').
+      joins(product_variants: [{ stock_item: :stock_movements }]).where('stock_movements.description != ?', 'Venta').
+      page(params[:page]).per(params[:per_page])
+    end
+    respond_to do |format| 
+            format.html { }
+            format.js  { respond_modal_index_with (@collection)}
+            format.xlsx {render xlsx: "reports", template: "admin/products/in_total.xlsx.axlsx"}
+    end 
+  end
+
+  def out_total
+    @page_description = 'Salidas'
+    @q = Product.ransack(params[:q])
+    unless params[:q].nil?
+      created_at_gt = params[:q][:created_at_gteq]
+      created_at_lt = params[:q][:created_at_lteq]
+
+      if params[:q][:created_at_gteq].present?
+        params[:q][:created_at_gteq] = Time.zone.parse(params[:q][:created_at_gteq]) rescue ""
+      end
+
+      if params[:q][:created_at_lteq].present?
+        params[:q][:created_at_lteq] = Time.zone.parse(params[:q][:created_at_lteq]) rescue ""
+      end
+      
+      @collection = @q.result(distinct: true).
+      select('products.*, stock_movements.folio, stock_movements.description, stock_movements.stock, stock_movements.quantity, stock_movements.cost_price, stock_movements.deleted_at, stock_movements.created_at').
+      joins(product_variants: [{ stock_item: :stock_movements }]).where('stock_movements.description = ?', 'Venta').
+      page(params[:page]).per(params[:per_page])
+    end
+    respond_to do |format| 
+            format.html { }
+            format.js  { respond_modal_index_with (@collection)}
+            format.xlsx {render xlsx: "reports", template: "admin/products/out_total.xlsx.axlsx"}
+    end 
+  end
 end
