01a7b9d5c79e707e565cd5ef114325d805e2fd3c
dova3187 <dovanny3187@gmail.com>
2021-06-10 16:39:53 -0500

    fix beginning date

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -30,19 +30,20 @@ class Admin::ReportsController < ApplicationController
 
   def service_orders_total
     @page_description = 'Ordenes de servicio'
-    @q = ServiceOrder.ransack(params[:q])
+    
     unless params[:q].nil?
       created_at_gt = params[:q][:created_at_gteq]
       created_at_lt = params[:q][:created_at_lteq]
 
       if params[:q][:created_at_gteq].present?
-        params[:q][:created_at_gteq] = Time.zone.parse(params[:q][:created_at_gteq]) rescue ""
+        params[:q][:created_at_gteq] = Time.zone.parse(params[:q][:created_at_gteq]).to_date.beginning_of_day rescue ""
       end
 
       if params[:q][:created_at_lteq].present?
-        params[:q][:created_at_lteq] = Time.zone.parse(params[:q][:created_at_lteq]) rescue ""
-      end
 
+        params[:q][:created_at_lteq] = Time.zone.parse(params[:q][:created_at_lteq]).to_date.end_of_day rescue ""
+      end
+      @q = ServiceOrder.ransack(params[:q])
       @collection = @q.result(:distinct => true).order('id DESC').includes(:customer).page(params[:page]).per(params[:per_page])
     end
     respond_to do |format| 
