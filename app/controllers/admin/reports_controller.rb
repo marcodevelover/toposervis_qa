add740990e8418ea0625e3bc83692b4d4ec71782
dova3187 <dovanny3187@gmail.com>
2021-10-19 11:46:08 -0500

    Update reports_controller.rb

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -198,7 +198,7 @@ class Admin::ReportsController < ApplicationController
     respond_to do |format| 
             format.html { }
             format.js  { respond_modal_index_with (@collection)}
-            #params[:per_page] = 10000
+            params[:per_page] = 10000
             format.xlsx {render xlsx: "reports", template: "admin/products/kardex.xlsx.axlsx"}
     end 
   end
